20d8591343f782b958caecd713b36dd9816e7df8
omarshooli1 <omarshooligabileytech@proton.me>
2026-06-16 22:26:04 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,12 +28,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
               <Link href="/vendors" className="rounded-full bg-slate-100 px-4 py-2 text-sm font-bold text-slate-700 hover:bg-slate-200">
                 Vendors
               </Link>
-              <Link href="/taxi" className="rounded-full bg-slate-100 px-4 py-2 text-sm font-bold text-slate-700 hover:bg-slate-200">
-                Taxi
-              </Link>
-              <Link href="/driver" className="rounded-full bg-slate-100 px-4 py-2 text-sm font-bold text-slate-700 hover:bg-slate-200">
-                Driver
-              </Link>
+            
               <Link href="/map" className="rounded-full bg-slate-100 px-4 py-2 text-sm font-bold text-slate-700 hover:bg-slate-200">
                 Map
               </Link>
